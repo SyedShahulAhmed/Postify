4e7735e161a3bc241d2aa6397222765a9cdcdb94
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-02-15 00:02:42 +0530

    Rectified Url Names

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
-    appwwrite_url : String(import.meta.env.VITE_APPWRITE_URL),
-    appwwrite_project_id : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwwrite_db_id : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwwrite_collection_id : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwwrite_bucket_id : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwrite_url : String(import.meta.env.VITE_APPWRITE_URL),
+    appwrite_project_id : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwrite_db_id : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwrite_collection_id : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwrite_bucket_id : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 
 export default config
