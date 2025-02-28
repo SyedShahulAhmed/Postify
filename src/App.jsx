a62db131c283862db009fcccac60fdc721c2bcc8
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-02-28 23:20:21 +0530

    Modfied App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import React from "react"
-import Sample from "./Components/Sample"
+import React, { useEffect, useState } from "react";
 
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login } from "./store/authSlice";
+import {Header,Footer} from './Components/index.js'
+// import { Outlet } from "react-router";
 function App() {
-  return (
-    <div>
-      <Sample/>
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((data) => {
+        if (data) {
+          dispatch(login({ data }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return loading ? 
+  <div className="text-xl text-blue-800">
+    <h1>Loading</h1>
+  </div> 
+  : 
+  <div className="min-h-screen  flex flex-wrap content-between bg-gray-600">
+    <div className="w-full block">
+        <Header/>
+        <div>
+          {/* <Outlet/> */}
+        </div>
+        <Footer />
     </div>
-  )
+  </div>;
 }
 
-export default App
+export default App;
