c4cfca63b1d8d8445039c9ffd0f692eb9a8a8633
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-03-08 18:01:00 +0530

    Added footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,33 +5,34 @@ import {
   AiFillLinkedin,
 } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router";
 const Footer = () => {
   return (
-    <div className="w-full bg-[#023e8a]">
-      <div className="max-w-[1240px] flex flex-col justify-between items-center mx-auto  px-2 p-5 lg:flex-row text-white gap-5">
-        <h1 className="font-semibold">Designed and Developed by <span className="text-[#2CEEF0]">Shahul Ahmed</span></h1>
-        <p className="font-semibold">Copyright © 2024 SA</p>
+    <div className="w-full bg-[#002137]">
+      <div className="max-w-[1240px] flex flex-col justify-between items-center mx-auto  px-2 p-5 lg:flex-row text-white gap-3">
+        <h1 className="font-semibold">Developed by <span className="text-orange-500">Shahul Ahmed</span></h1>
+        <p className="font-semibold">Copyright © <span className="text-orange-500">2025 SA</span></p>
         <ul className="flex justify-center items-center gap-5">
-          {/* <Link to={'https://github.com/SyedShahulAhmed'}> */}
-            <li className="hover:text-[#171515] transition-colors duration-300">
+          <Link to={'https://github.com/SyedShahulAhmed'}>
+            <li className="hover:text-orange-500 transition-colors duration-300">
               <AiFillGithub size={20} />
             </li>
-          {/* </Link> */}
-          {/* <Link to={'https://x.com/ShahulAhmed17?t=Kk665GXRLquOrucnSvkTew&s=09'}> */}
-            <li className="hover:text-[#1DA1F2] transition-colors duration-300">
+          </Link>
+          <Link to={'https://x.com/ShahulAhmed17?t=Kk665GXRLquOrucnSvkTew&s=09'}>
+            <li className="hover:text-orange-500 transition-colors duration-300">
               <FaXTwitter size={20}/>
             </li>
-          {/* </Link> */}
-          {/* <Link to={'https://www.linkedin.com/in/shahul-ahmed/'}> */}
-            <li className="hover:text-[#0077B5] transition-colors duration-300">
+          </Link>
+          <Link to={'https://www.linkedin.com/in/shahul-ahmed/'}>
+            <li className="hover:text-yellow-500 transition-colors duration-300">
               <AiFillLinkedin size={20} />
             </li>
-          {/* </Link> */}
-          {/* <Link to={'https://www.instagram.com/vincenzo_773/'}> */}
-            <li className="hover:text-[#E1306C] transition-colors duration-300">
+          </Link>
+          <Link to={'https://www.instagram.com/vincenzo_773/'}>
+            <li className="hover:text-orange-500 transition-colors duration-300">
               <AiFillInstagram size={20} />
             </li>
-          {/* </Link> */}
+          </Link>
         </ul>
       </div>
     </div>
